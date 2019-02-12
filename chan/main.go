341ba20d2c8b7b4d438196542759241f19e45fe5
bjxxlvxin <bjlvxin@jd.com>
2019-02-12 11:41:53 +0800

    add chan test code

---
+++ chan/main.go
@@ -0,0 +1,47 @@
+/*
+Copyright 2019 Tiger
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreedto in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+created by lvxin for project golang-test at 19-2-12 上午11:39
+*/
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	stop := make(chan bool)
+
+	go func() {
+		for {
+			select {
+			case <-stop:
+				fmt.Println("监控退出，停止了...")
+				return
+			default:
+				fmt.Println("goroutine监控中...")
+				time.Sleep(1 * time.Second)
+			}
+		}
+	}()
+	fmt.Println("服务启动成功，运行中...")
+	time.Sleep(10 * time.Second)
+	fmt.Println("可以了，通知监控停止")
+	stop<- true
+	//为了检测监控过是否停止，如果没有监控输出，就表示停止了
+	time.Sleep(5 * time.Second)
+
+}
